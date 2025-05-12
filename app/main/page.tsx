841fed745d51a403fd3c2209090134ec9a6e87b9
s-erzv <sarahfajriarahmah@gmail.com>
2025-05-12 15:53:45 +0700

    add main page

---
+++ app/main/page.tsx
@@ -0,0 +1,26 @@
+// app/main/page.tsx
+'use client'
+
+import AyatQuran from '../components/AyatQuran'
+import CoupleSection from '../components/CoupleSection'
+import EventSection from '../components/EventSection'
+import Footer from '../components/Footer'
+import MusicPlayer from '../components/MusicPlayer'
+import Doa from '../components/Doa'
+import Hero from '../components/Hero'
+import WeddingGift from '../components/WeddingGift'
+
+export default function MainPage() {
+  return (
+    <main>
+      <MusicPlayer />
+      <Hero />
+      <AyatQuran />
+      <CoupleSection />
+      <EventSection />
+      <Doa />
+      <WeddingGift />
+      <Footer />
+    </main>
+  )
+}
